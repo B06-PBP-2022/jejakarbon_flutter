def552e99a6f51a045610a512a9e35d7364da686
pantunelfreddy <pantun.elfreddy@ui.ac.id>
2022-12-10 01:29:42 +0700

    routing halaman payment

--- lib/apps/buka_donasi/page/open_donasi_page.dart
+++ lib/apps/buka_donasi/page/open_donasi_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:jejakarbon_flutter/components/drawer.dart';
 import 'package:jejakarbon_flutter/apps/buka_donasi/util/fetch_daftar_donasi.dart';
 import 'package:getwidget/getwidget.dart';
+import 'package:jejakarbon_flutter/apps/pembayaran/payment.dart';
 
 void main() {
   runApp(const BukaDonasiPage());
@@ -115,7 +116,11 @@ class _BukaDonasiState extends State<BukaDonasiPage> {
                                             },
                                           ),
                                         ),
-                                        onPressed: () {},
+                                        onPressed: () {
+                                          Navigator.push(
+                                            context, MaterialPageRoute(builder: (context) => PaymentMethod())
+                                        );
+                                        },
                                         child: const Text('Donasi')),
                                   ],
                                 ),
---
+++ lib/apps/pembayaran/payment.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const PaymentMethod());
+}
+
+class PaymentMethod extends StatefulWidget {
+  const PaymentMethod({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _PaymentMethodState();
+}
+
+class _PaymentMethodState extends State<PaymentMethod> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Payment"),
+      ),
+    );
+  }
+
+
+}
