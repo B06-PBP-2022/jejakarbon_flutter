6046dcd7c07d9da98795667ca9af2167f750acc4
dipa.alhaza <dipa.alhaza@ui.ac.id>
2022-12-09 22:35:48 +0700

    edit drawer

--- lib/apps/faq/page/faq_page.dart
+++ lib/apps/faq/page/faq_page.dart
@@ -21,145 +21,155 @@ class _FaqPageState extends State<FaqPage> {
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Frequently Asked Questions"),
-      ),
-      drawer: buildDrawer(context),
-      // future builder berisi data watchlist
-      body: FutureBuilder(
-        future: fetchWatchList(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.data == null) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            if (!snapshot.hasData) {
-              return Column(
-                children: const [
-                  Text(
-                    "Pertanyaan belum ada? Tanyakan pada form berikut ini",
-                    style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
-                  ),
-                  SizedBox(height: 8),
-                ],
-              );
+        appBar: AppBar(
+          title: const Text("Frequently Asked Questions"),
+        ),
+        drawer: buildDrawer(context),
+        // future builder berisi data watchlist
+        body: FutureBuilder(
+          future: fetchWatchList(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return const Center(child: CircularProgressIndicator());
             } else {
-              return Container(
-                padding: EdgeInsets.symmetric(vertical: 20),
-                  child: ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (_, index) => GestureDetector(
-                            child: Accordion(
-                              maxOpenSections: 2,
-                              headerBackgroundColor: Color.fromARGB(255, 119, 130, 119),
-                              scaleWhenAnimating: true,
-                              openAndCloseAnimation: true,
-                              headerPadding: const EdgeInsets.symmetric(
-                                  vertical: 15, horizontal: 15),
-                              sectionOpeningHapticFeedback:
-                                  SectionHapticFeedback.heavy,
-                              sectionClosingHapticFeedback:
-                                  SectionHapticFeedback.light,
-                              paddingListBottom: 2,
-                              paddingListTop: 2,
-                              children: [
-                                AccordionSection(
-                                    isOpen: false,
-                                    header: Text(
-                                      "${snapshot.data![index].fields.question}",
-                                      style: const TextStyle(
-                                          fontSize: 16.0,
-                                          color: Colors.white),
-                                    ),
-                                    content: Column(children: [
-                                      Container(
-                                          alignment: Alignment.topLeft,
-                                          padding: const EdgeInsets.all(10),
-                                          child: Badge(
-                                            badgeContent: Text(
-                                              "asked by ${snapshot.data![index].fields.username}",
-                                              overflow: TextOverflow.visible,
-                                              style: const TextStyle(
-                                                  fontWeight: FontWeight.bold,
-                                                  color: Colors.white),
-                                            ),
-                                            shape: BadgeShape.square,
-                                            badgeColor: Colors.green,
-                                            borderRadius:
-                                                BorderRadius.circular(5),
-                                          )),
-                                      Column(children: [
-                                        Container(
-                                          alignment: Alignment.topLeft,
-                                          padding: const EdgeInsets.symmetric(horizontal:10),
-                                          child:Text("Jawaban:",
-                                            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green),),
-                                        ),
-                                        Container(
-                                        padding: const EdgeInsets.symmetric(horizontal:10),
-                                        alignment: Alignment.topLeft,
-                                        child: Text(
-                                          "${snapshot.data![index].fields.answer}",
-                                          overflow: TextOverflow.visible,
-                                        ),
-                                      )
-                                      ],)
-                                    ]))
-                              ],
-                            ),
-                          )
-                          )
+              if (!snapshot.hasData) {
+                return Column(
+                  children: const [
+                    Text(
+                      "Pertanyaan belum ada? Tanyakan pada form berikut ini",
+                      style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                    ),
+                    SizedBox(height: 8),
+                  ],
                 );
+              } else {
+                return Container(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (_, index) => GestureDetector(
+                              child: Accordion(
+                                maxOpenSections: 2,
+                                headerBackgroundColor:
+                                    Color.fromARGB(255, 119, 130, 119),
+                                scaleWhenAnimating: true,
+                                openAndCloseAnimation: true,
+                                headerPadding: const EdgeInsets.symmetric(
+                                    vertical: 15, horizontal: 15),
+                                sectionOpeningHapticFeedback:
+                                    SectionHapticFeedback.heavy,
+                                sectionClosingHapticFeedback:
+                                    SectionHapticFeedback.light,
+                                paddingListBottom: 2,
+                                paddingListTop: 2,
+                                children: [
+                                  AccordionSection(
+                                      isOpen: false,
+                                      header: Text(
+                                        "${snapshot.data![index].fields.question}",
+                                        style: const TextStyle(
+                                            fontSize: 16.0,
+                                            color: Colors.white),
+                                      ),
+                                      content: Column(children: [
+                                        Container(
+                                            alignment: Alignment.topLeft,
+                                            padding: const EdgeInsets.all(10),
+                                            child: Badge(
+                                              badgeContent: Text(
+                                                "asked by ${snapshot.data![index].fields.username}",
+                                                overflow: TextOverflow.visible,
+                                                style: const TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.white),
+                                              ),
+                                              shape: BadgeShape.square,
+                                              badgeColor: Colors.green,
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                            )),
+                                        Column(
+                                          children: [
+                                            Container(
+                                              alignment: Alignment.topLeft,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 10),
+                                              child: Text(
+                                                "Jawaban:",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.green),
+                                              ),
+                                            ),
+                                            Container(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 10),
+                                              alignment: Alignment.topLeft,
+                                              child: Text(
+                                                "${snapshot.data![index].fields.answer}",
+                                                overflow: TextOverflow.visible,
+                                              ),
+                                            )
+                                          ],
+                                        )
+                                      ]))
+                                ],
+                              ),
+                            )));
+              }
             }
-          }
-        },
-      ),
-      floatingActionButton: request.loggedIn ? Container(
-          padding: const EdgeInsets.only(left: 30),
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: TextButton(
-              style: ButtonStyle(
-                  minimumSize: MaterialStateProperty.all(Size(100, 50)),
-                  backgroundColor: MaterialStateProperty.all(Colors.green)),
-              onPressed: () {
-                Navigator.push(context, 
-                  MaterialPageRoute(builder: ((context) => FaqForm())
-                ));
-              },
-              child: const Text(
-                'Ask a question here',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18
+          },
+        ),
+        floatingActionButton: request.loggedIn
+            ? Container(
+                padding: const EdgeInsets.only(left: 30),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: TextButton(
+                    style: ButtonStyle(
+                        minimumSize: MaterialStateProperty.all(Size(100, 50)),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.green)),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: ((context) => FaqForm())));
+                    },
+                    child: const Text(
+                      'Ask a question here',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ),
-        ) : Container(
-          padding: const EdgeInsets.only(left: 30),
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: TextButton(
-              style: ButtonStyle(
-                  minimumSize: MaterialStateProperty.all(Size(100, 50)),
-                  backgroundColor: MaterialStateProperty.all(Colors.green)),
-              onPressed: () {
-                Navigator.push(context, 
-                  MaterialPageRoute(builder: ((context) => LoginPage())
-                ));
-              },
-              child: const Text(
-                'Login to add question!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18
+              )
+            : Container(
+                padding: const EdgeInsets.only(left: 30),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: TextButton(
+                    style: ButtonStyle(
+                        minimumSize: MaterialStateProperty.all(Size(100, 50)),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.green)),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: ((context) => LoginPage())));
+                    },
+                    child: const Text(
+                      'Login to add question!',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ),
-        )
-    );
+              ));
   }
 }
