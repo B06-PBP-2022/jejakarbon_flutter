5028514a26f30404c900745ac91673990f511e77
dipa.alhaza <dipa.alhaza@ui.ac.id>
2022-12-11 14:03:43 +0700

    add new fitur

--- lib/apps/buka_donasi/page/edit.dart
+++ lib/apps/buka_donasi/page/edit.dart
@@ -1,41 +1,69 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:jejakarbon_flutter/apps/buka_donasi/model/buka_donasi.dart';
+import 'package:jejakarbon_flutter/apps/buka_donasi/page/open_donasi_page.dart';
 
 import 'package:jejakarbon_flutter/components/drawer/drawer.dart';
 import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:intl/intl.dart';
 
-class BukaDonasiForm extends StatefulWidget {
-  const BukaDonasiForm({super.key});
+class EditDonasiForm extends StatefulWidget {
+  DaftarDonasi target;
+
+  EditDonasiForm({super.key, required this.target});
 
   @override
-  State<BukaDonasiForm> createState() => _BukaDonasiFormState();
+  State<EditDonasiForm> createState() =>
+      _BukaDonasiFormState(targetEdit: target);
 }
 
-class _BukaDonasiFormState extends State<BukaDonasiForm> {
+class _BukaDonasiFormState extends State<EditDonasiForm> {
+  DaftarDonasi targetEdit;
+  _BukaDonasiFormState({required this.targetEdit});
+
   final _formKey = GlobalKey<FormState>();
   final _controller = TextEditingController();
-
-  
-
   static const _locale = 'idr';
+
+  var _judul = TextEditingController();
+  var _deskripsi = TextEditingController();
+  var _target = TextEditingController();
+
+  // String judul = " ";
+  // String deskripsi = " ";
+  // String targetDonasi = " ";
+
+  void initState() {
+    super.initState();
+    int j = targetEdit.fields.targetDonasi;
+    _judul.text = targetEdit.fields.temaKegiatan;
+    _deskripsi.text = targetEdit.fields.deskripsi;
+    _target.text = "${j}";
+  }
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   String _formatNumber(String s) =>
       NumberFormat.decimalPattern(_locale).format(int.parse(s));
   String get _currency =>
       NumberFormat.compactSimpleCurrency(locale: _locale).currencySymbol;
 
-  String judul = " ";
-  String deskripsi = " ";
-  String targetDonasi = " ";
-
   @override
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
+    // judul = targetEdit.fields.temaKegiatan;
+    // deskripsi = targetEdit.fields.deskripsi;
+    // int j = targetEdit.fields.targetDonasi;
+    // targetDonasi = "${j}";
+
     return Scaffold(
         appBar: AppBar(
-          title: Text('Buka Donasi'),
+          title: Text('Edit Donasi'),
         ),
         drawer: buildDrawer(context),
         body: Form(
@@ -50,6 +78,7 @@ class _BukaDonasiFormState extends State<BukaDonasiForm> {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: TextFormField(
+                      controller: _judul,
                       decoration: InputDecoration(
                         hintText: "Judul . . . ",
                         labelText: "Judul Kegiatan",
@@ -61,12 +90,12 @@ class _BukaDonasiFormState extends State<BukaDonasiForm> {
                       ),
                       onChanged: (String? value) {
                         setState(() {
-                          judul = value!;
+                          _judul.text = value!;
                         });
                       },
                       onSaved: (String? value) {
                         setState(() {
-                          judul = value!;
+                          _judul.text = value!;
                         });
                       },
                       autovalidateMode: AutovalidateMode.onUserInteraction,
@@ -81,6 +110,7 @@ class _BukaDonasiFormState extends State<BukaDonasiForm> {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: TextFormField(
+                      controller: _deskripsi,
                       minLines: 6,
                       maxLines: 8,
                       decoration: InputDecoration(
@@ -94,12 +124,12 @@ class _BukaDonasiFormState extends State<BukaDonasiForm> {
                       ),
                       onChanged: (String? value) {
                         setState(() {
-                          deskripsi = value!;
+                          _deskripsi.text = value!;
                         });
                       },
                       onSaved: (String? value) {
                         setState(() {
-                          deskripsi = value!;
+                          _deskripsi.text = value!;
                         });
                       },
                       autovalidateMode: AutovalidateMode.onUserInteraction,
@@ -114,6 +144,7 @@ class _BukaDonasiFormState extends State<BukaDonasiForm> {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: TextFormField(
+                      controller: _target,
                       inputFormatters: [
                         FilteringTextInputFormatter.allow(
                           RegExp(r"[0-9]"),
@@ -131,12 +162,12 @@ class _BukaDonasiFormState extends State<BukaDonasiForm> {
                       ),
                       onChanged: (String? value) {
                         setState(() {
-                          targetDonasi = value!;
+                          _target.text = value!;
                         });
                       },
                       onSaved: (String? value) {
                         setState(() {
-                          targetDonasi = value!;
+                          _target.text = value!;
                         });
                       },
                       autovalidateMode: AutovalidateMode.onUserInteraction,
@@ -155,38 +186,45 @@ class _BukaDonasiFormState extends State<BukaDonasiForm> {
         ),
         floatingActionButton: Container(
           padding: const EdgeInsets.only(left: 30),
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: TextButton(
-              style: ButtonStyle(
-                  minimumSize: MaterialStateProperty.all(Size(100, 50)),
-                  backgroundColor: MaterialStateProperty.all(Colors.green)),
-              onPressed: () async {
-                final response = await request.postJson(
-                    "https://jejakarbon.up.railway.app/form-pembuatan-donasi/open-donasi-flutter/",
-                    jsonEncode({
-                      "tema_kegiatan": judul,
-                      "deskripsi": deskripsi,
-                      "target_donasi": targetDonasi,
-                    }));
-                // print(response);
-                Navigator.pop(context);
-                // ignore: use_build_context_synchronously
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const BukaDonasiForm()),
-                );
-              },
-              child: const Text(
-                'Submit',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18),
-              ),
-            ),
-          ),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                FloatingActionButton.extended(
+                  heroTag: 'uniqueTag',
+                  label: Row(
+                    children: [Icon(Icons.arrow_back)],
+                  ),
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const BukaDonasiPage()),
+                    );
+                  },
+                  // child: Icon(Icons.delete_sharp),
+                ),
+                FloatingActionButton.extended(
+                    onPressed: () async {
+                      // ignore: use_build_context_synchronously
+                      final response = await request.postJson(
+                          "https://jejakarbon.up.railway.app/form-pembuatan-donasi/edit-event/${widget.target.pk}/",
+                          jsonEncode({
+                            "tema_kegiatan": _judul.text,
+                            "deskripsi": _deskripsi.text,
+                            "target_donasi": _target.text,
+                          }));
+                      // print(response);
+                      // ignore: use_build_context_synchronously
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const BukaDonasiPage()),
+                      );
+                    },
+                    foregroundColor: Colors.white,
+                    heroTag: 'uniqueTag',
+                    label: Row(children: [Icon(Icons.save)])),
+              ]),
         ));
   }
 }
