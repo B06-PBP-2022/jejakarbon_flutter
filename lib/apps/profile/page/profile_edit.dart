8c1a40826c34b490067e0d44bd53ab269cb143f8
daffarestupratama <daffa.ilham@ui.ac.id>
2022-12-12 13:52:32 +0700

    finish edit profile features

--- lib/apps/profile/page/profile_edit.dart
+++ lib/apps/profile/page/profile_edit.dart
@@ -5,7 +5,8 @@ import 'package:pbp_django_auth/pbp_django_auth.dart';
 
 class ProfileEditPage extends StatefulWidget {
   String title;
-  ProfileEditPage({super.key, required this.title});
+  ProfileEditPage({super.key, required this.title, this.reqAuthenticated});
+  CookieRequest? reqAuthenticated;
 
   @override
   State<ProfileEditPage> createState() => _ProfileEditPageState();
@@ -105,7 +106,78 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
                       // print(usernameResponse['available']);
 
                       if (usernameResponse['available'] == true) {
-                        print("YES");
+                        // print("YES");
+
+                        // Update username
+                        // if (widget.reqAuthenticated != null) {
+                        //   print("reqAuthenticated is not null");
+                        // } else {
+                        //   print("reqAuthenticated is null");
+                        // }
+                        var updateUsernameResponse = await request.post(
+                          'https://jejakarbon.up.railway.app/profile/change-username-flutter/',
+                          {
+                            "username": username,
+                          },
+                        );
+
+                        if (updateUsernameResponse['username'] == username) {
+                          // Edit username success
+                          BuildContext dialogContext;
+                          showDialog(
+                            context: context,
+                            // barrierDismissible: false,
+                            builder: (context) {
+                              dialogContext = context;
+                              return Dialog(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                elevation: 15,
+                                child: Container(
+                                  child: ListView(
+                                    padding: const EdgeInsets.only(
+                                        top: 20, bottom: 20),
+                                    shrinkWrap: true,
+                                    children: <Widget>[
+                                      Center(
+                                          child: Column(
+                                        children: const [
+                                          Text(
+                                            "Username berhasil diubah!",
+                                            style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ],
+                                      )),
+                                      const SizedBox(height: 20),
+                                      TextButton(
+                                        onPressed: () {
+                                          // Navigator.pop(context);
+                                          // setState(() {
+                                          //   _judul = "";
+                                          //   _nominal = "";
+                                          //   _jenis = "Pemasukan";
+                                          // });
+                                          Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const ProfilePage(),
+                                            ),
+                                          );
+                                        },
+                                        child: const Text('OK'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        }
                       } else {
                         BuildContext dialogContext;
                         showDialog(
@@ -158,27 +230,256 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
                       }
 
                       break;
-                    case 'Ganti Kontak':
-                      var contactResponse = await request.post(
-                          'https://jejakarbon.up.railway.app/profile/change-contact-flutter/',
-                          {
-                            "contact": _input,
-                          });
 
-                      print(contactResponse);
-                      if (contactResponse['contact'] == _input) {
-                        print("YES");
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const ProfilePage()),
-                        );
+                    case 'Ganti Kontak':
+                      if (int.tryParse(_input) != null) {
+                        var contactResponse = await request.post(
+                            'https://jejakarbon.up.railway.app/profile/change-contact-flutter/',
+                            {
+                              "contact": _input,
+                            });
+                        // print(contactResponse);
+
+                        if (contactResponse['contact'] == _input) {
+                          // Edit username success
+                          BuildContext dialogContext;
+                          showDialog(
+                            context: context,
+                            // barrierDismissible: false,
+                            builder: (context) {
+                              dialogContext = context;
+                              return Dialog(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                elevation: 15,
+                                child: Container(
+                                  child: ListView(
+                                    padding: const EdgeInsets.only(
+                                        top: 20, bottom: 20),
+                                    shrinkWrap: true,
+                                    children: <Widget>[
+                                      Center(
+                                          child: Column(
+                                        children: const [
+                                          Text(
+                                            "Kontak berhasil diubah!",
+                                            style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ],
+                                      )),
+                                      const SizedBox(height: 20),
+                                      TextButton(
+                                        onPressed: () {
+                                          // Navigator.pop(context);
+                                          // setState(() {
+                                          //   _judul = "";
+                                          //   _nominal = "";
+                                          //   _jenis = "Pemasukan";
+                                          // });
+                                          Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const ProfilePage(),
+                                            ),
+                                          );
+                                        },
+                                        child: const Text('OK'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        }
                       } else {
-                        print("NO");
+                        // input is not a number
+                        BuildContext dialogContext;
+                        showDialog(
+                          context: context,
+                          // barrierDismissible: false,
+                          builder: (context) {
+                            dialogContext = context;
+                            return Dialog(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              elevation: 15,
+                              child: Container(
+                                child: ListView(
+                                  padding: const EdgeInsets.only(
+                                      top: 20, bottom: 20),
+                                  shrinkWrap: true,
+                                  children: <Widget>[
+                                    Center(
+                                        child: Column(
+                                      children: const [
+                                        Text(
+                                          "Input bukan angka!",
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    )),
+                                    const SizedBox(height: 20),
+                                    TextButton(
+                                      onPressed: () {
+                                        // Navigator.pop(context);
+                                        // setState(() {
+                                        //   _judul = "";
+                                        //   _nominal = "";
+                                        //   _jenis = "Pemasukan";
+                                        // });
+                                        Navigator.pop(dialogContext);
+                                      },
+                                      child: const Text('OK'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
                       }
+
+                      // if (contactResponse['contact'] == _input) {
+                      //   print("YES");
+                      //   Navigator.pushReplacement(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //         builder: (context) => const ProfilePage()),
+                      //   );
+                      // } else {
+                      //   print("NO");
+                      // }
 
                       break;
                     case 'Ganti Email':
+                      if (RegExp(
+                              r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$")
+                          .hasMatch(_input)) {
+                        // print("VALID");
+                        var emailResponse = await request.post(
+                            'https://jejakarbon.up.railway.app/profile/change-email-flutter/',
+                            {
+                              "email": _input,
+                            });
+
+                        if (emailResponse['email'] == _input) {
+                          // Edit username success
+                          BuildContext dialogContext;
+                          showDialog(
+                            context: context,
+                            // barrierDismissible: false,
+                            builder: (context) {
+                              dialogContext = context;
+                              return Dialog(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                elevation: 15,
+                                child: Container(
+                                  child: ListView(
+                                    padding: const EdgeInsets.only(
+                                        top: 20, bottom: 20),
+                                    shrinkWrap: true,
+                                    children: <Widget>[
+                                      Center(
+                                          child: Column(
+                                        children: const [
+                                          Text(
+                                            "Email berhasil diubah!",
+                                            style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ],
+                                      )),
+                                      const SizedBox(height: 20),
+                                      TextButton(
+                                        onPressed: () {
+                                          // Navigator.pop(context);
+                                          // setState(() {
+                                          //   _judul = "";
+                                          //   _nominal = "";
+                                          //   _jenis = "Pemasukan";
+                                          // });
+                                          Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const ProfilePage(),
+                                            ),
+                                          );
+                                        },
+                                        child: const Text('OK'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        }
+                      } else {
+                        // print("INVALID");
+                        BuildContext dialogContext;
+                        showDialog(
+                          context: context,
+                          // barrierDismissible: false,
+                          builder: (context) {
+                            dialogContext = context;
+                            return Dialog(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              elevation: 15,
+                              child: Container(
+                                child: ListView(
+                                  padding: const EdgeInsets.only(
+                                      top: 20, bottom: 20),
+                                  shrinkWrap: true,
+                                  children: <Widget>[
+                                    Center(
+                                        child: Column(
+                                      children: const [
+                                        Text(
+                                          "Email yang dimasukkan tidak valid!",
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    )),
+                                    const SizedBox(height: 20),
+                                    TextButton(
+                                      onPressed: () {
+                                        // Navigator.pop(context);
+                                        // setState(() {
+                                        //   _judul = "";
+                                        //   _nominal = "";
+                                        //   _jenis = "Pemasukan";
+                                        // });
+                                        Navigator.pop(dialogContext);
+                                      },
+                                      child: const Text('OK'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      }
                       break;
                   }
                 }
